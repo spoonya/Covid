bf21f84a780cf7831368f9c13aae971dd2950c27
ortophius@mail.ru <ortophius@mail.ru>
2020-12-21 17:08:41 +0400

    feat: implement country borders highlighting

--- src/ts/components/map.component.ts
+++ src/ts/components/map.component.ts
@@ -6,14 +6,40 @@ import * as Api from '../data/api.data';
 import REQUESTS from '../constants/api.requests.const';
 import prettifyNumber from '../helpers/pretiffy.number.helper';
 
+interface FeatureProps {
+  FIPS: String;
+  ISO2: String;
+  ISO3: String;
+  UN: String;
+  NAME: String;
+  AREA: String;
+  POP2005: String;
+  REGION: String;
+  SUBREGION: String;
+  LON: Number;
+  LAT: Number;
+  units: String;
+  count: String;
+  saturation: Number;
+}
+
+interface Geometry {
+  type: String;
+  coordinates: [number, number][];
+}
+
+interface Feature {
+  type: String;
+  properties: FeatureProps;
+  geometry: Geometry;
+}
+
 const ApiData = new Api.ApiData();
 
 export default class Map {
   protected map: L.Map;
 
-  private totalMarkers: L.LayerGroup;
-
-  private per100Markers: L.LayerGroup;
+  private borderLayer: L.GeoJSON;
 
   constructor(parent: HTMLElement) {
     const options = {
@@ -22,8 +48,23 @@ export default class Map {
     };
 
     this.map = L.map(parent, options).setView([44.37256049721892, 2.039828672758004], 3);
-    this.totalMarkers = L.layerGroup();
-    this.per100Markers = L.layerGroup();
+
+    this.borderLayer = L.geoJSON();
+
+    this.borderLayer.options.style = (borderFeature) => {
+      const fillColor = `hsl(${borderFeature?.properties.saturation}, 100%, 50%)`;
+
+      return {
+        color: 'white',
+        fillColor,
+      };
+    };
+
+    this.borderLayer.options.onEachFeature = (borderFeature, layer) => {
+      const popupText = `<b>Name:</b> ${borderFeature.properties.NAME}<br><b>${borderFeature.properties.units}:</b> ${borderFeature.properties.count}`;
+
+      layer.bindPopup(popupText);
+    };
   }
 
   init() {
@@ -36,29 +77,39 @@ export default class Map {
   }
 
   async update(): Promise<void> {
+    this.borderLayer.clearLayers();
+
     const countries = await ApiData.getCovidCountries(REQUESTS.countries);
 
-    const maxCases = Math.max.apply(
-      null,
-      countries.map((country) => country.cases),
+    const maxCases = Math.log2(
+      Math.max.apply(
+        null,
+        countries.map((country) => country.cases),
+      ),
     );
 
-    await Promise.all(
-      countries.map(async (country) => {
-        const latlng = [country.countryInfo.lat, country.countryInfo.long];
-        const maxMarkerSize = 70;
-        const markerRadius = (country.cases / maxCases) * maxMarkerSize;
+    const req = await fetch('/static/borders.json');
+    const borders = await req.json();
 
-        L.circleMarker(latlng as L.LatLngExpression, {
-          color: 'red',
-          fillColor: 'red',
-          radius: markerRadius,
-        })
-          .addTo(this.totalMarkers)
-          .bindPopup(`<b>${country.country}</b><br> <b>Total cases:</b> ${prettifyNumber(country.cases)}`);
-      }),
-    );
+    borders.features
+      .filter((feature: Feature) => {
+        const countryInfo = countries.filter(
+          (countryObj) => countryObj.countryInfo.iso3 === feature.properties.ISO3,
+        )[0];
+        return !!countryInfo;
+      })
+      .forEach((feature: Feature) => {
+        const countryInfo = countries.filter(
+          (countryObj) => countryObj.countryInfo.iso3 === feature.properties.ISO3,
+        )[0];
 
-    this.totalMarkers.addTo(this.map);
+        feature.properties.units = 'Cases';
+        feature.properties.count = prettifyNumber(countryInfo.cases);
+        feature.properties.saturation = 100 - (Math.log2(countryInfo.cases) / maxCases) * 100;
+      });
+
+    this.borderLayer.addData(borders);
+
+    this.borderLayer.addTo(this.map);
   }
 }
