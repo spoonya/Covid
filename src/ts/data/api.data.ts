745e47ea93a07b227fcd378a89f80d6cda282147
ortophius@mail.ru <ortophius@mail.ru>
2020-12-23 13:47:27 +0400

    feat: implement api response caching

--- src/ts/data/api.data.ts
+++ src/ts/data/api.data.ts
@@ -68,48 +68,70 @@ export interface CovidCountries {
   readonly criticalPerOneMillion: number;
 }
 
-export class ApiData {
-  public getCovidCountries = async (): Promise<CovidCountries[]> => {
-    try {
-      const res = await fetch('https://disease.sh/v3/covid-19/countries');
-      const data: CovidCountries[] = await res.json();
+interface StorageData {
+  data: object;
+  timestamp: number;
+}
 
-      return data;
+export class ApiData {
+  private responseCacheTime: number = 60 * 5; // 5 minutes
+
+  private checkStorage(key: string): boolean {
+    const res = window.localStorage.getItem(key);
+
+    if (!res) return false;
+
+    const storageData: StorageData = JSON.parse(res);
+    const currentDate = new Date().getTime();
+    const timespan = Math.floor((currentDate - new Date(storageData.timestamp).getTime()) / 1000);
+
+    if (timespan > this.responseCacheTime) return false;
+
+    return true;
+  }
+
+  private saveToStorage = (key: string, data: any): void => {
+    const timestamp = new Date().getTime();
+    const dataToSave = { data, timestamp };
+    window.localStorage.setItem(key, JSON.stringify(dataToSave));
+  };
+
+  private loadFromStorage = (key: string): object => {
+    const storageData: StorageData = JSON.parse(window.localStorage.getItem(key)!);
+    if (!storageData) return {};
+    return storageData.data;
+  };
+
+  private async fetchUrl(storageKey: string, url: string): Promise<void> {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      this.saveToStorage(storageKey, data);
     } catch (error) {
       throw new Error(error);
     }
+  }
+
+  public getCovidCountries = async (): Promise<CovidCountries[]> => {
+    if (!this.checkStorage('countries')) await this.fetchUrl('countries', 'https://disease.sh/v3/covid-19/countries');
+    return this.loadFromStorage('countries') as CovidCountries[];
   };
 
   public getCovidSummary = async (): Promise<CovidSummary> => {
-    try {
-      const res = await fetch('https://disease.sh/v3/covid-19/all');
-      const data: CovidSummary = await res.json();
-
-      return data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    if (!this.checkStorage('summary')) await this.fetchUrl('summary', 'https://disease.sh/v3/covid-19/all');
+    return this.loadFromStorage('summary') as CovidSummary;
   };
 
   public getCovidHistory = async (): Promise<CovidHistory> => {
-    try {
-      const res = await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=366');
-      const data: CovidHistory = await res.json();
-
-      return data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    if (!this.checkStorage('history'))
+      await this.fetchUrl('history', 'https://disease.sh/v3/covid-19/historical/all?lastdays=366');
+    return this.loadFromStorage('history') as CovidHistory;
   };
 
   public getCovidCountryHistory = async (country: string): Promise<CovidCountryHistory> => {
-    try {
-      const res = await fetch(`https://disease.sh/v3/covid-19/historical/${country}?lastdays=366`);
-      const data: CovidCountryHistory = await res.json();
-
-      return data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    if (!this.checkStorage(`history-${country}`))
+      await this.fetchUrl(`history-${country}`, `https://disease.sh/v3/covid-19/historical/${country}?lastdays=366`);
+    return this.loadFromStorage(`history-${country}`) as CovidCountryHistory;
   };
 }
